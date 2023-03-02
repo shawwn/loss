b8f3a54dacb088491fc61a037c7c876856bfeef2
Shawn Presser <shawnpresser@gmail.com>
2023-03-02 02:10:40 -0600

    Initial engine

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,10 +1,19 @@
 import unittest
 
 import loss
+import loss.engine.common.math as m
+import loss.engine.graphics as g
 
 class TestCase(unittest.TestCase):
   def test_basic(self):
     self.assertEqual(1, 1)
+
+  def test_add(self):
+    vec = m.MVec3(1, 1, 1)
+    self.assertEqual(vec, vec)
+    self.assertEqual(vec + vec - vec, vec)
+    self.assertGreater(vec + vec, vec)
+
 
 if __name__ == '__main__':
   unittest.main()
